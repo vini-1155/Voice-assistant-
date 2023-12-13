98c8af9b4b1ba1187024c37ddcceb7994209ca2c
unknown <Vinay M Devang>
2023-12-13 19:37:26 +0530

    hindiai

--- lanhindi.py
+++ lanhindi.py
@@ -6,6 +6,9 @@ from googletrans import Translator
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 
+def say(text):
+    engine.say(text)
+    engine.runAndWait()
 
 def speak(text):
     engine = pyttsx3.init()
@@ -15,12 +18,20 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-
-t = Translator()
 r = sr.Recognizer()
+
+def translate_hindi_to_english(hindi_text):
+                translator = Translator()
+                print("Translating...")
+                # Translate from Hindi to English
+                translation = translator.translate(hindi_text, src='hi', dest='en')
+                # print(translation)
+                # Return the translated text
+                return (translation.text)
 
 
 if __name__ == "__main__":
+
     speak("नमस्ते")
     with sr.Microphone() as source:
         # speak("नमस्ते आप क्या")
@@ -29,8 +40,11 @@ if __name__ == "__main__":
         try:
             command = r.recognize_google(audio, language="hi-in")
             speak(command)
-            translator = Translator()
-            translation = translator.translate(command, dest='en')
-            print(translation.text)
+            # Example usage
+            # hindi_text = "यह हिंदी टेक्स्ट को अंग्रेजी में अनुवाद करें"
+            english_text = translate_hindi_to_english(command)
+            say(english_text)
+            print(english_text)
+
         except Exception as e:
             print(e)
