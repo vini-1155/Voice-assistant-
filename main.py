ae3d14041581dd816bc5de3408c77cc39cf93084
vinay-devang <vinay.20201cse0604@presidencyuniversity.in>
2023-12-15 18:50:10 +0530

    updated-enai

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
+import openai
+# from config import apikey
+from openai import OpenAI
 import speech_recognition as sr
 import os
 import pyttsx3
 
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
 
-'''
-def speak(audio):
-speak("Hello Vinay!!")
-'''
+def ai(userquery):
+    try:
+        client = OpenAI(
+            api_key="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")
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=[
+                {
+                    "role": "user",
+                    "content": userquery
+                }
+            ],
+            temperature=1,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        # print(response.choices)
+        generated_texts = [
+            choice.message.content for choice in response.choices
+        ]
+        say(''.join(generated_texts))
+        print(''.join(generated_texts))
+
+    except Exception as e:
+        print(e)
 
 
 def say(text):
@@ -33,7 +59,10 @@ def takecommand():
 
 
 if __name__ == '__main__':
-    # say("Hello I am Guna how can I help You")
     while True:
         print("Listening...")
-        text = takecommand()
+        query = takecommand()
+        if "alpha".lower() in query.lower():
+            quer = query.lower()
+            query = quer.replace('alpha', "Health-AI")
+            ai(userquery=query)
