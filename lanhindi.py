919f467e94f7a28c5ba81feb027e1240e2ee270e
vinay-devang <vinay.20201cse0604@presidencyuniversity.in>
2023-12-15 19:57:15 +0530

    hindi_ai

--- lanhindi.py
+++ lanhindi.py
@@ -1,12 +1,16 @@
 import pyttsx3
+import openai
+# from config import apikey
+from openai import OpenAI
 import speech_recognition as sr
 from googletrans import Translator
-
+from deep_translator import GoogleTranslator
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 
 def say(text):
+    engine.setProperty('voice', voices[3].id)
     engine.say(text)
     engine.runAndWait()
 
@@ -14,24 +18,54 @@ def speak(text):
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)
-    engine.setProperty('rate', 160)
+    engine.setProperty('rate', 180)
     engine.say(text)
     engine.runAndWait()
 
 r = sr.Recognizer()
 
-def translate_hindi_to_english(hindi_text):
-                translator = Translator()
-                print("Translating...")
-                # Translate from Hindi to English
-                translation = translator.translate(hindi_text, src='hi', dest='en')
-                # print(translation)
-                # Return the translated text
-                return (translation.text)
+def ai(userquery):
+    try:
+        client = OpenAI(
+            api_key="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")
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=[
+                {
+                    "role": "user",
+                    "content": userquery
+                }
+            ],
+            temperature=1,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
 
+        # print(response.choices)
+        generated_texts = [
+            choice.message.content for choice in response.choices
+        ]
+
+        # say(''.join(generated_texts))
+        # print(''.join(generated_texts))
+        trans_hindi_to_eng(''.join(generated_texts))
+
+
+
+    except Exception as e:
+        print(e)
+def trans_hindi_to_eng(eng):
+    trans = GoogleTranslator(source="english", target="hindi").translate(eng)
+    speak(trans)   
+
+def translate_hindi_to_english(hindi_text):
+                trans = GoogleTranslator(source="hindi", target="english").translate(hindi_text)
+                return trans
+     
 
 if __name__ == "__main__":
-
     speak("नमस्ते")
     with sr.Microphone() as source:
         # speak("नमस्ते आप क्या")
@@ -43,8 +77,7 @@ if __name__ == "__main__":
             # Example usage
             # hindi_text = "यह हिंदी टेक्स्ट को अंग्रेजी में अनुवाद करें"
             english_text = translate_hindi_to_english(command)
-            say(english_text)
-            print(english_text)
+            ai(english_text)
 
         except Exception as e:
             print(e)
