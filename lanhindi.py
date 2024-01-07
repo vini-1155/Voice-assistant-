1fd86e7b3ef8dabf6d6283a3f2078d5acc5918df
vinay-devang <vinay.20201cse0604@presidencyuniversity.in>
2024-01-07 11:09:56 +0530

    final-commit

--- lanhindi.py
+++ lanhindi.py
@@ -2,7 +2,6 @@ import pyttsx3
 import openai
 from openai import OpenAI
 import speech_recognition as sr
-from deep_translator import GoogleTranslator
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -25,7 +24,7 @@ r = sr.Recognizer()
 def ai(userquery):
     try:
         client = OpenAI(
-            api_key="")
+            api_key="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")
         response = client.chat.completions.create(
             model="gpt-3.5-turbo-1106",
             messages=[
@@ -46,37 +45,21 @@ def ai(userquery):
             choice.message.content for choice in response.choices
         ]
         res = ''.join(generated_texts)
-        # say(res)
-        # print(''.join(generated_texts))
-        # trans_english_to_regional(''.join(generated_texts))
         print("ai",res)
         return res
 
     except Exception as e:
-        print(e)
+        print(e) 
 
+# if __name__ == "__main__":
+#     speak("नमस्ते")
+#     with sr.Microphone() as source:
+#         # speak("नमस्ते आप क्या")
+#         print("Listening...")
+#         audio = r.listen(source)
+#         try:
+#             command = r.recognize_google(audio, language="hi-in")
+#             speak(command)
 
-def translate_regional_to_english(hindi_text):
-                trans = GoogleTranslator(source="hindi", target="english").translate(hindi_text)
-                print(trans)
-                return trans
-     
-def trans_english_to_regional(eng):
-    trans = GoogleTranslator(source="english", target="hindi").translate(eng)
-    print(trans)
-    speak(trans)   
-
-if __name__ == "__main__":
-    speak("नमस्ते")
-    with sr.Microphone() as source:
-        # speak("नमस्ते आप क्या")
-        print("Listening...")
-        audio = r.listen(source)
-        try:
-            command = r.recognize_google(audio, language="hi-in")
-            speak(command)
-            english_text = translate_regional_to_english(command)
-            ai(english_text)
-
-        except Exception as e:
-            print(e)
+#         except Exception as e:
+#             print(e)
