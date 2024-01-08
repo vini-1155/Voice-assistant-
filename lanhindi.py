4189ad70d8029223a3d5d4a85e78446ca4f7d0ca
Vinay M <104661631+vini-1155@users.noreply.github.com>
2024-01-08 09:50:40 +0530

    Update lanhindi.py

--- lanhindi.py
+++ lanhindi.py
@@ -1,30 +1,11 @@
-import pyttsx3
 import openai
 from openai import OpenAI
 import speech_recognition as sr
 
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-
-def say(text):
-    engine.setProperty('voice', voices[3].id)
-    engine.say(text)
-    engine.runAndWait()
-
-def speak(text):
-    engine = pyttsx3.init()
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id)
-    engine.setProperty('rate', 180)
-    engine.say(text)
-    engine.runAndWait()
-
-r = sr.Recognizer()
-
 def ai(userquery):
     try:
         client = OpenAI(
-            api_key="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")
+            api_key="")
         response = client.chat.completions.create(
             model="gpt-3.5-turbo-1106",
             messages=[
@@ -39,27 +20,12 @@ def ai(userquery):
             frequency_penalty=0,
             presence_penalty=0
         )
-
-        # print(response.choices)
         generated_texts = [
             choice.message.content for choice in response.choices
         ]
         res = ''.join(generated_texts)
-        print("ai",res)
+        print("Alpha : ",res)
         return res
 
     except Exception as e:
         print(e) 
-
-# if __name__ == "__main__":
-#     speak("नमस्ते")
-#     with sr.Microphone() as source:
-#         # speak("नमस्ते आप क्या")
-#         print("Listening...")
-#         audio = r.listen(source)
-#         try:
-#             command = r.recognize_google(audio, language="hi-in")
-#             speak(command)
-
-#         except Exception as e:
-#             print(e)
